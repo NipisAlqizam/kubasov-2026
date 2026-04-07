c27b626882674560d4b2ec1e2bd606d95019327d
Broken Timer <konkin.anton2005123123123@gmail.com>
2026-04-07 13:29:43 +0300

    поправили поверхность на d11, чтобы было без резких переходов

--- 3d_rang_papapa/LiNb.m
+++ 3d_rang_papapa/LiNb.m
@@ -25,7 +25,7 @@ d_matrix_new = dtensor2matrix(d_tensor_new);
 theta = 0:2:360; %поворот вокруг x1
 phi = 0: 2: 180; %потворот вокруг x2'
 
-d33 = zeros(length(theta),length(phi));
+d11 = zeros(length(theta),length(phi));
 % заготовка для значений продольного коэффициента
 tic
 for i=1:length(theta)
@@ -36,20 +36,20 @@ for i=1:length(theta)
     % считаем всю матрицу на каждой итерации, каждый коэффициент из 27
     % d_tensor_new=d_tensor_rotate(d_tensor, rot_matrix);
 
-    %d33(i,j)=d_tensor_new(3,3,3);
+    %d11(i,j)=d_tensor_new(3,3,3);
 
     % 1 оптимизация
-    % d_tensor_new = d_tensor_d33_rotate(d_tensor, rot_matrix);
-    % d33(i,j) = d_tensor_new(3,3,3);
+    % d_tensor_new = d_tensor_d11_rotate(d_tensor, rot_matrix);
+    % d11(i,j) = d_tensor_new(3,3,3);
 
     % оптимизированный вариант через матричные умножения
     %d_tensor_new = d_tensor_rotate_fast(d_tensor, rot_matrix);
 
-    %d33(i, j) = d_tensor_new(3,3,3);
+    %d11(i, j) = d_tensor_new(3,3,3);
 
     % оптимизированный вариант без перехода к тензору
     d_matrix_new = dmatrix_rotate(d_matrix, rot_matrix);
-    d33(i,j) = d_matrix_new(3,3);
+    d11(i,j) = d_matrix_new(1,1);
 
   end
 end
@@ -58,10 +58,10 @@ toc
 % сетка для построения поверхности
 [PHI, THETA] = meshgrid(deg2rad(phi), deg2rad(theta));
 % переходим в декартову ск
-[X, Y, Z] = sph2cart(THETA, -(PHI-pi/2), abs(d33));
+[X, Y, Z] = sph2cart(THETA, -(PHI-pi/2), abs(d11));
 
 figure
-surf (X, Y, Z, d33);
+surf (X, Y, Z, d11);
 shading interp
 colorbar
 daspect([1, 1, 1]);
@@ -70,8 +70,8 @@ daspect([1, 1, 1]);
 % построим цветовую карту продольного пьезомодуля
 
 figure
-surf(rad2deg(THETA),rad2deg(PHI),d33);
-[X, Y, Z] = sph2cart(THETA, -(PHI-pi/2), abs(d33));
+surf(rad2deg(THETA),rad2deg(PHI),d11);
+[X, Y, Z] = sph2cart(THETA, -(PHI-pi/2), abs(d11));
 daspect([1 1 1]);
 shading interp;       % сглаживаем цвета
 
